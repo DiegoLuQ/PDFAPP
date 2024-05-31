b5026a728cf041f9415a6fe4d4be589ab1d6fe97
Diego <juan.luque@colegiomacaya.cl>
2024-05-31 15:35:32 -0400

    agregano link de pdf al metodo post

--- app/main.py
+++ app/main.py
@@ -8,12 +8,14 @@ app = FastAPI()
 # Directorio base para los archivos PDF
 BASE_DIR = Path("static")
 
+
 @app.get("/")
 def read_root():
     return {"message": "Go to /pdf?filename=sample.pdf to view the PDF"}
 
+
 @app.get("/pdf")
-async def get_pdf(filename:str):
+async def get_pdf(filename: str):
     # Construir la ruta completa al archivo de manera segura
     pdf_path = Path('.') / 'static' / filename
 
@@ -24,11 +26,13 @@ async def get_pdf(filename:str):
     # Devolver el archivo PDF
     return FileResponse(pdf_path, media_type='application/pdf')
 
+
 @app.post("/upload")
 async def upload_pdf(file: UploadFile = File(...)):
     # Verificar que el archivo tiene la extensión .pdf
     if not file.filename.endswith(".pdf"):
-        raise HTTPException(status_code=400, detail="Only PDF files are allowed/Solo archivos PDF son permitidos")
+        raise HTTPException(
+            status_code=400, detail="Only PDF files are allowed/Solo archivos PDF son permitidos")
 
     # Construir la ruta completa al archivo donde se guardará
     file_path = BASE_DIR / file.filename
@@ -37,7 +41,11 @@ async def upload_pdf(file: UploadFile = File(...)):
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    return {"filename": file.filename, "message": "File uploaded successfully/Archivo Subido exitosamente"}
+    return {"filename": file.filename, 
+            "message": "File uploaded successfully/Archivo Subido exitosamente", 
+            "link": f"https://pdf.luque.site/pdf?filename={file.filename}"
+            }
+
 
 @app.get("/list-pdfs")
 async def list_pdfs():
@@ -45,6 +53,7 @@ async def list_pdfs():
     pdf_files = [str(file.name) for file in BASE_DIR.glob("*.pdf")]
 
     return {"pdf_files": pdf_files}
+
 
 @app.delete("/delete-pdf")
 async def delete_pdf(filename: str):
